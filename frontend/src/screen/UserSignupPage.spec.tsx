8ceb13e63fd40c16cfe31ca938d60e404767742f
Ashley <thy.ashley@gmail.com>
2020-12-09 00:29:24 +0800

    added fields for test

--- frontend/src/screen/UserSignupPage.spec.tsx
+++ frontend/src/screen/UserSignupPage.spec.tsx
@@ -1,40 +1,72 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import UserSignupPage from "./UserSignupPage";
 
-describe("UserSignupPage layout", () => {
+describe("Test UserSignupPage", () => {
   beforeEach(() => {
     render(<UserSignupPage />);
   });
-  it("should contain a header with text sign up", () => {
-    const header = screen.getByTestId("header");
-    expect(header).toBeInTheDocument();
-    expect(header).toHaveTextContent(/sign up/i);
+  describe("Layouts", () => {
+    it("should contain a header with text sign up", () => {
+      const header = screen.getByTestId("header");
+      expect(header).toBeInTheDocument();
+      expect(header).toHaveTextContent(/sign up/i);
+    });
+    it("should contain a input field for name", () => {
+      const inputField = screen.getByTestId("nameInput");
+      expect(inputField).toBeInTheDocument();
+      expect(inputField).toHaveAttribute("placeholder", "Enter your name");
+    });
+    it("should contain a input field for username", () => {
+      const inputField = screen.getByTestId("usernameInput");
+      expect(inputField).toBeInTheDocument();
+      expect(inputField).toHaveAttribute("placeholder", "Enter your username");
+    });
+    it("should contain a input field for password with type password", () => {
+      const inputField = screen.getByTestId("passwordInput");
+      expect(inputField).toBeInTheDocument();
+      expect(inputField).toHaveAttribute("placeholder", "Enter your password");
+      expect(inputField).toHaveAttribute("type", "password");
+    });
+    it("should contain a input field for confirm password with type password", () => {
+      const inputField = screen.getByTestId("confirmPasswordInput");
+      expect(inputField).toBeInTheDocument();
+      expect(inputField).toHaveAttribute(
+        "placeholder",
+        "Confirm your password"
+      );
+      expect(inputField).toHaveAttribute("type", "password");
+    });
+    it("should contain a input field for register button", () => {
+      const Button = screen.getByTestId("registerButton");
+      expect(Button).toBeInTheDocument();
+      expect(Button).toHaveTextContent(/register/i);
+    });
   });
-  it("should contain a input field for name", () => {
-    const inputField = screen.getByTestId("nameInput");
-    expect(inputField).toBeInTheDocument();
-    expect(inputField).toHaveAttribute("placeholder", "Enter your name");
-  });
-  it("should contain a input field for username", () => {
-    const inputField = screen.getByTestId("usernameInput");
-    expect(inputField).toBeInTheDocument();
-    expect(inputField).toHaveAttribute("placeholder", "Enter your username");
-  });
-  it("should contain a input field for password with type password", () => {
-    const inputField = screen.getByTestId("passwordInput");
-    expect(inputField).toBeInTheDocument();
-    expect(inputField).toHaveAttribute("placeholder", "Enter your password");
-    expect(inputField).toHaveAttribute("type", "password");
-  });
-  it("should contain a input field for confirm password with type password", () => {
-    const inputField = screen.getByTestId("confirmPasswordInput");
-    expect(inputField).toBeInTheDocument();
-    expect(inputField).toHaveAttribute("placeholder", "Confirm your password");
-    expect(inputField).toHaveAttribute("type", "password");
-  });
-  it("should contain a input field for register button", () => {
-    const Button = screen.getByTestId("registerButton");
-    expect(Button).toBeInTheDocument();
-    expect(Button).toHaveTextContent(/register/i);
+  describe("Functionalities", () => {
+    const createEvent = (content: string | number) => {
+      return {
+        target: {
+          value: content,
+        },
+      };
+    };
+    it("should allow user to key in their name", () => {
+      const changeEvent = createEvent("user1");
+      const inputField = screen.getByTestId("nameInput");
+      fireEvent.change(inputField, changeEvent);
+      expect(inputField).toHaveValue(changeEvent.target.value);
+    });
+    it("should allow user to key in their username", () => {
+      const changeEvent = createEvent("user1");
+      const inputField = screen.getByTestId("usernameInput");
+      fireEvent.change(inputField, changeEvent);
+      expect(inputField).toHaveValue(changeEvent.target.value);
+    });
+    it("should allow user to key in their password", () => {
+      const changeEvent = createEvent("P4ssword");
+      const inputField = screen.getByTestId("passwordInput");
+      fireEvent.change(inputField, changeEvent);
+      expect(inputField).toHaveValue(changeEvent.target.value);
+    });
   });
 });
--- frontend/src/screen/UserSignupPage.tsx
+++ frontend/src/screen/UserSignupPage.tsx
@@ -1,20 +1,37 @@
 import React from "react";
+import { useState } from "react";
 
 const UserSignupPage = () => {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   return (
     <div>
       <h1 data-testid="header">Sign up</h1>
       <div>
-        <input data-testid="nameInput" placeholder="Enter your name" />
+        <input
+          data-testid="nameInput"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div>
-        <input data-testid="usernameInput" placeholder="Enter your username" />
+        <input
+          data-testid="usernameInput"
+          placeholder="Enter your username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
       <div>
         <input
           data-testid="passwordInput"
           placeholder="Enter your password"
           type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
       </div>
       <div>
@@ -22,6 +39,8 @@ const UserSignupPage = () => {
           data-testid="confirmPasswordInput"
           placeholder="Confirm your password"
           type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />
       </div>
       <button data-testid="registerButton">Register</button>
